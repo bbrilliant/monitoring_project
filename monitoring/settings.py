fdb785ca60977780115f7bd102f70fd6e791a1be
Bright Amegnaglo <bamegnaglo@gmail.com>
2025-10-02 09:23:38 +0000

    Bug fix 1

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),  # si tu veux un dossier "static/" global
 ]
 
+SECURE_CROSS_ORIGIN_OPENER_POLICY=None
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
