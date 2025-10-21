348d918a9c0131f421a2c8371c005be16a69f53d
Bright Amegnaglo <bamegnaglo@gmail.com>
2025-10-21 17:35:56 +0000

    Checking checked once more again

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -184,5 +184,5 @@ EMAIL_PORT = env.int('EMAIL_PORT', default=587)
 EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=True)
 EMAIL_HOST_USER = env('EMAIL_HOST_USER', default='noreply.atos.dev@gmail.com')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD', default='fgdasnfvnitrlaiy')
-DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='API Monitor <noreply.atos.dev@gmail.com>')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL', default='API Monitor - Dashboard <noreply.atos.dev@gmail.com>')
 
